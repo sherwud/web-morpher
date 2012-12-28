feb4c1acebb1794c7dff409937bb853361c02cab
IDrakon <maildrakon@gmail.com>
2012-12-28 17:01:20 +0400

    ... =)

--- web-morpher/index.js
+++ web-morpher/index.js
@@ -1,23 +1,66 @@
 /*
  * Модуль сервера
  */
+/* Параметры сервера по умолчанию */
+var defaultRootPath = '../demo/';
+var defaultConfigFile = 'config.json';
+var defaultPort = 3000;
+/* Конструктор */
+var $wm = exports = module.exports = wmConstructor;
+/* парсинг параметров запуска */
+$wm.startArgs = {};
+for (var i in global.process.argv) {
+   if (global.process.argv[i].search('--') !== -1) {
+      var val = global.process.argv[i].replace(/--/,'').split(':');
+      $wm.startArgs[val[0]]=val[1];
+   }
+}
+/* чтение данных из package.json*/
+var packageInfo = require('./package.json');
+$wm.info = {};
+$wm.info.name = packageInfo.name;
+$wm.info.version = packageInfo.version;
+$wm.info.versionName = packageInfo.versionName;
+$wm.info.author = packageInfo.author;
+delete packageInfo;
+
+function wmConstructor(rootPath,configFile,port) {
+   var wm = {};
+   var globalConfigs = require('./config.json');
+   wm.rootPath =
+      $wm.startArgs.path?$wm.startArgs.path:
+      rootPath?rootPath:
+      defaultRootPath;
+   if (wm.rootPath[wm.rootPath.length-1]!=='/') wm.rootPath+='/';
+   wm.wmPath = wm.rootPath+'web-morpher/'
+   wm.configFile =
+      $wm.startArgs.config?$wm.startArgs.config:
+      configFile?configFile:
+      defaultConfigFile;
+   
+   var configs = require(wm.wmPath+wm.configFile);
+   
+   
+   wm.port =
+      $wm.startArgs.port?$wm.startArgs.port:
+      port?port:
+      defaultPort;
+   wm.start = wmStart;
+   return wm;
+}
+
+function wmStart(){
+   console.log('Корень сайта: '+this.rootPath);
+   console.log('Сервер запущен на порту: '+this.port);
+}
+
+/*
 wm = {};
 module.exports = wmConstructor();
-function wmConstructor() {
-   /* парсинг параметров запуска */
-   function parseArgv(){
-      var argv = global.process.argv;
-      var arg = {};
-      for (var i in argv) {
-         if (argv[i].search('--') !== -1) {
-            var val = argv[i].replace(/--/,'').split(':');
-            arg[val[0]]=val[1];
-         }
-      }
-      return arg;
-   }
+function wmConstructorOLD() {
 
-   /* параметры */
+
+   // параметры
    var args = parseArgv();
    var configs = require('./'+(args.config || 'config.json'));
    if (configs.paths instanceof Array) {
@@ -28,7 +71,7 @@ function wmConstructor() {
    var port = args.port || configs.port || 3000;
    var packageInfo = require('./package.json');
 
-   /* запуск сервера */
+   // запуск сервера
    var express = require('express');
    var app = express();
    app.use(express.logger());
@@ -38,7 +81,7 @@ function wmConstructor() {
    });   
    app.listen(port);
 
-   /* вывод */
+   // вывод
    if (args.config)
       console.log('Использован файл настроек: '+args.config);
    console.log('Сервер запущен на порту: '+port);
@@ -56,3 +99,4 @@ function wmConstructor() {
    delete packageInfo;
    return wm;
 }
+*/
