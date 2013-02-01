968d413e553f5f4c1a88303b53d8967ad0ea2f8a
IDrakon <maildrakon@gmail.com>
2013-02-01 22:30:38 +0400

    docs

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ console.log('Параметры запуска:');
 console.log(wmConstructor.startArgs);
 console.log('Информация о сервере:');
 console.log(wmConstructor.info);
-var demo = wmConstructor({path:'demo'});
+var demo = wmConstructor({path:'demo',port:778});
 var docs = wmConstructor({path:'docs',port:777});
 demo.start();
 docs.start();
