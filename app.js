b2a4f0321d80e811715447c449cdbc928f058c69
IDrakon <maildrakon@gmail.com>
2013-02-11 12:52:33 +0400

    core

--- app.js
+++ app.js
@@ -1,8 +1,4 @@
 /* инициализация Web morpher */
 var wmConstructor = require('./web-morpher');
-console.log('Параметры запуска:');
-console.log(wmConstructor.startArgs);
-console.log('Информация о сервере:');
-console.log(wmConstructor.info);
 var wm = wmConstructor({path:'docs',port:777});
 wm.start();
