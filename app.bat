f335e93e2fdf45553b3696d1d35458f61ca08197
Dr@KoN <maildrakon@gmail.com>
2013-07-02 22:21:39 +0400

    test

--- app.bat
+++ app.bat
@@ -1,1 +1,1 @@
-node --harmony --harmony_typeof app.js
+node --harmony --harmony_typeof app.js --path:./sites/test
--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 "use strict";
+var args = {};
+for (var i in global.process.argv) {
+   if (global.process.argv[i].search(/^--/) !== -1) {
+      var val = global.process.argv[i].replace(/--/,'').split(':');
+      args[val[0]]=val[1];
+   }
+}
 require('./web-morpher');
 wmlog(wm.config.info());
-wmlog(wm.app('./sites/test'));
+wmlog(wm.app(args.path));
 wmlog('well done!');
