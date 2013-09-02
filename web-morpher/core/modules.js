39a046037dad1afe24477113d3156744be1bcd1a
IDrakon <maildrakon@gmail.com>
2013-09-02 15:52:02 +0400

    wm.modules

--- web-morpher/core/modules.js
+++ web-morpher/core/modules.js
@@ -1,8 +1,14 @@
 "use strict";
 var wmpath = wm.path;
-if ('siteroot' in wmpath)
-   exports = module.exports = wmabstract(wmpath.siteroot+'/dynamic/modules');
-else {
+if ('siteroot' in wmpath) {
+   try{
+      exports = module.exports =
+         wmabstract(wmpath.siteroot+'/dynamic/modules',null,true);
+   }catch(e){
+      exports = module.exports = {};
+      wmlog(e,{'title':'export modules'});
+   }
+} else {
    exports = module.exports = {};
    wmlog('Свойство "siteroot" не найдено в объекте "wm.path"',
       {'title':'export modules'}
