2f59797aafa5241e16da08b284aaf5effc31ce72
ia.novozhilov <ia.novozhilov@tsd-novozhilov.corp.tensor.ru>
2013-10-18 14:02:08 +0400

    рефакторинг

--- web-morpher/core/path.js
+++ web-morpher/core/path.js
@@ -1,6 +1,6 @@
 "use strict";
 var path = wm.ext.path;
 exports = module.exports = {};
-exports.wm = path.dirname(path.normalize(module.parent.parent.filename));
-exports.root = path.dirname(exports.wm);
+exports.wmroot = path.dirname(path.normalize(module.parent.parent.filename));
+exports.root = path.dirname(exports.wmroot);
 exports.startupdir = path.dirname(path.normalize(module.parent.parent.parent.filename));
