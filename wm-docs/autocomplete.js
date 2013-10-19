987c0fbdf6b93dbea818b6117fad6346f7161f7d
Dr@KoN <maildrakon@gmail.com>
2013-10-19 11:54:02 +0400

    docs

--- wm-docs/autocomplete.js
+++ wm-docs/autocomplete.js
@@ -1,23 +1,33 @@
 /**
- * Глобальный объект web-morpher, обеспечивающий доступ ко всему функционалу системы
+ * @description Глобальный объект web-morpher
+ * , обеспечивающий доступ ко всему функционалу системы.
  * @syntax wm
  */
-var wm = {};
+var wm = {
+   /**
+    * @description Глобальный конфиг системы web-morpher.
+    * @syntax wm.config
+    */
+   config:{}
+};
 /**
- * Общий системный метод логирования.<br>
- * console.log не может логировать объекты от конструктора <b>wmabstract</b>
+ * @description Общий системный метод логирования.
+ * @description console.log не может логировать объекты класса wmabstract.
  * @syntax wmlog(msg,prm)
  * @param {any} msg
  * @param {Object} prm
  */
 var wmlog = function(msg,prm){};
-
-
-/* Шаблон */
 /**
- * Многострочный
- *    коментраний к коду c html тегами
- * @syntax Синтаксис вызова (жиный текст в заголовке окна подсказки)
- * @param {Тип} ИмяПеременной - входные параметры метода (1 запись 1 параметр)
- * @returns {Тип возвращаемого значения}
+ * @description Конструктор для абстрактных объектов.
+ * @syntax newobj = wmabstract(modPath,modLogic,critical)
+ * @param {String/Array} modPath путь к файлам модуля
+ * @param {String} modLogic логическое расположение объекта
+ * @param {Boolean} critical при ошибке: t - кидать throw, f - писать в лог 
+ * @return {Object}
+ * @example global.wm = wmabstract('./core','wm');
+ * @example var myObj.myOpt = wmabstract('./myDir','myObj.myOpt');
+ * @example var myObj = wmabstract(['./m1','./m2'],'myObj');
+ * @name namePath
  */
+var wmabstract = function(modPath,modLogic,critical){};
