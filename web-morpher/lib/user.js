8abcee156c394786cbd52562878d08186bae244e
Dr@KoN <maildrakon@gmail.com>
2013-10-30 23:36:20 +0400

    рефакторинг

--- web-morpher/lib/user.js
+++ web-morpher/lib/user.js
@@ -1,30 +1,36 @@
 "use strict";
 var path = wm.ext.path;
 var util = wm.util;
-var shared = path.join(wm.path.wmroot,'front-end','shared');
+var shared = path.join(wm.path.wmroot, 'front-end', 'shared');
 module.exports = userAuth;
 module.exports.__isProxy = true;
 var users = {};
 var config = wm.server.config.users;
 function userAuth(req, res, next) {
-   function auth(){
-      function verify(isAuth){
-         if (isAuth === true) return success(users[uuid]);
-         if (isAuth === false) return res.send(403,'403 Forbidden');
+   function auth() {
+      function verify(isAuth) {
+         if (isAuth === true) {
+            return success(users[uuid]);
+         }
+         if (isAuth === false) {
+            return res.send(403, '403 Forbidden');
+         }
          if (config.guest) {
             return success(users[uuid] = {name: 'Гость', role: 'guest'});
          }
-         return res.sendfile(path.join(shared,'login.html'));
+         return res.sendfile(path.join(shared, 'login.html'));
       }
       return verify();
    }
-   function success(user){
+   function success(user) {
       req.user = user;
       req.user.lastLogin = new Date;
       next();
    }
    var uuid = req.session.uuid;
-   if (uuid in users) { return success(users[uuid]); }
+   if (uuid in users) {
+      return success(users[uuid]);
+   }
    uuid = req.session.uuid = uuid || util.generateUUID();
    auth();
 }
