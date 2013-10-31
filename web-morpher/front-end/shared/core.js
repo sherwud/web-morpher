25a19ef3e6adaf3e6a6f875e74dcd4fe23949a61
Dr@KoN <maildrakon@gmail.com>
2013-10-31 09:29:20 +0400

    системные интерфейсы "начало"

--- web-morpher/front-end/shared/core.js
+++ web-morpher/front-end/shared/core.js
@@ -1,7 +1,18 @@
 window.$wm = new function() {
    this.ajax = ajax;
 
-   function ajax(options) {
-
+   function ajax(options, callback) {
+      var http = new XMLHttpRequest();
+      http.open(options.method, options.url, true);
+      if ('function' === typeof callback) {
+         http.onreadystatechange = function() {
+            if (http.readyState == 4) {
+               if (http.status == 200) {
+                  callback(http.responseText);
+               }
+            }
+         };
+      }
+      http.send(options.data || null);
    }
 };
