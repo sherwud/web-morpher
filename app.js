488f828bc4b31373446a8e5215434d7b7d041cdd
Dr@KoN <maildrakon@gmail.com>
2013-11-01 23:15:53 +0400

    рефакторинг

--- app.js
+++ app.js
@@ -2,12 +2,12 @@
 var args = {};
 for (var i in global.process.argv) {
    if (~global.process.argv[i].search(/^--/)) {
-      var val = global.process.argv[i].replace(/--/,'').split(':');
-      args[val[0]]=val[1];
+      var val = global.process.argv[i].replace(/--/, '').split(':');
+      args[val[0]] = val[1];
    }
 }
 require('./web-morpher');
 //wmlog(wm.info(),{title:'О системе',code:2});
-wm.app(args.path,function(){
-   wmlog('well done!',{code:0});
+wm.app(args.path, function() {
+   wmlog('well done!', {code: 0});
 });
