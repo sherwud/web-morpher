15b5d80893fdc184f5d3be9c669579356fb299f0
Dr@KoN <maildrakon@gmail.com>
2013-11-06 10:48:58 +0400

    тестовая шарилка музыки (HTML5 only)

--- projects/sharemusic/resource/dynamic/init.js
+++ projects/sharemusic/resource/dynamic/init.js
@@ -1,13 +1,10 @@
 "use strict";
 var path = wm.ext.path;
+var dir = 'E:/Музыка';
 exports = module.exports = function(server){
-   server.app.get('/',function(req,res){
-      var file = path.normalize(req.query.file);
-      var test = file.split(path.sep);
-      if (test[0]==='E:' && test[1]==='Музыка'){
-         res.sendfile(file);
-      } else {
-         res.send(403,'Доступ запрещен');
-      }
+   server.app.use(server.express.directory(dir));
+   server.app.get('/*',function(req,res){
+      var file = path.normalize(path.join(dir,req.url));
+      res.sendfile(file);
    });
 };
